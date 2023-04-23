67a0a0efabe8a38b9b8e314890af8bad579eecfe
janymaro <janyel4ds@gmail.com>
2023-04-23 14:20:54 -0400

    first commit

---
+++ data_import.R
@@ -0,0 +1,14 @@
+library(tidyverse)
+
+read_csv("a,b,c
+         1,2,3
+         4,5,6")
+
+read_csv("One line of metadata
+         Another line of metadata
+         x,y,z
+         1,2,3", skip = 2)
+
+read_csv("# A comment
+         x,y,z
+         1,2,3", comment = "#")
